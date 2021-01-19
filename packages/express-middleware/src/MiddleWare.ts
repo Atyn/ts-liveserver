ad40fe70f014cb820d31a4e57f7662bd13bd1d45
atyn <atynatyn@gmail.com>
2021-01-19 14:00:41 +0100

    WIP

--- packages/express-middleware/src/MiddleWare.ts
+++ packages/express-middleware/src/MiddleWare.ts
@@ -2,23 +2,54 @@ import Express from 'express'
 import Path from 'path'
 import { TsTranspiler } from '@ts-liveserver/ts-transpiler'
 
-const tsTranspiler = new TsTranspiler()
-
-export default async function MiddleWare(
-	request: Express.Request,
-	response: Express.Response,
-	next: () => void,
-) {
-	switch (Path.extname(request.path)) {
-		case '.tsx':
-		case '.ts':
-		case '.js':
-		case '.jsx':
-			response.set({ 'Content-Type': 'application/javascript' })
-			const fileName = Path.resolve('.' + request.path)
-			response.send(await tsTranspiler.transformFile(fileName))
-			break
-		default:
-			return next()
+export default class MiddleWare {
+	private path: string
+	private tsTranspiler = new TsTranspiler()
+	constructor(path = '.') {
+		this.path = path
+	}
+	async onRequest(
+		request: Express.Request,
+		response: Express.Response,
+		next: () => void,
+	) {
+		switch (Path.extname(request.path)) {
+			case '.tsx':
+			case '.ts':
+			case '.js':
+			case '.jsx':
+				response.set({ 'Content-Type': 'application/javascript' })
+				const fileName = Path.resolve(this.path + request.path)
+				response.send(await this.tsTranspiler.transformFile(fileName))
+				break
+			default:
+				return next()
+		}
+		return undefined
 	}
 }
+
+/*
+export default function MiddleWare(path = '.') {
+	const tsTranspiler = new TsTranspiler()
+	return async function (
+		request: Express.Request,
+		response: Express.Response,
+		next: () => void,
+	) {
+		switch (Path.extname(request.path)) {
+			case '.tsx':
+			case '.ts':
+			case '.js':
+			case '.jsx':
+				response.set({ 'Content-Type': 'application/javascript' })
+				const fileName = Path.resolve(path + request.path)
+				response.send(await tsTranspiler.transformFile(fileName))
+				break
+			default:
+				return next()
+		}
+		return undefined
+	}
+}
+*/
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -3,6 +3,8 @@
 import Express from 'express'
 import MiddleWare from '@ts-liveserver/express-middleware'
 
+const path = process.argv[2]
+const middleWare = new MiddleWare(path)
 const app = Express()
 const port = 8080
 
@@ -10,5 +12,5 @@ app.listen(port, () => {
 	console.log('Server listening for port', port)
 })
 
-app.use(MiddleWare)
-app.use(Express.static('.'))
+app.use(middleWare.onRequest)
+app.use(Express.static(path))
