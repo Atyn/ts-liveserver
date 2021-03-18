a70713792084228ad7f7010760586a96e80f93f8
atyn <atynatyn@gmail.com>
2021-03-18 14:02:53 +0100

    More changes

--- packages/ts-transpiler/__tests__/TsTranspiler.test.ts
+++ packages/ts-transpiler/__tests__/TsTranspiler.test.ts
@@ -6,9 +6,11 @@ describe('TsTranspiler', () => {
 	it('Transpile a module', async () => {
 		const inputFileName = './ModuleA.tsx'
 		const transpiler = new TsTranspiler()
-		const results = await transpiler.transformFile(
-			Path.resolve(__dirname, 'inputData', inputFileName),
-		)
+		const results = (
+			await transpiler.transformFile(
+				Path.resolve(__dirname, 'inputData', inputFileName),
+			)
+		).outputText
 		expect(results + '\n').toBe(
 			Fs.readFileSync(
 				Path.resolve(__dirname, 'outputData', 'ModuleA.js'),
