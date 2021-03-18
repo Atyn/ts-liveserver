fe000f5596aee45fe4b6aa8df13b3287817ec506
atyn <atynatyn@gmail.com>
2021-03-18 21:48:18 +0100

    Added lots of CommonJS syntax support

--- packages/ts-transpiler/__tests__/outputData/ModuleA.js
+++ packages/ts-transpiler/__tests__/outputData/ModuleA.js
@@ -1,6 +1,6 @@
 import ModuleB from "./ModuleB.ts";
 const component = React.createElement("div", null, "Hello");
-component;
+console.log(component);
 const ModuleA = ModuleB;
 export default ModuleA;
 //# sourceMappingURL=ModuleA.js.map
--- packages/ts-transpiler/src/transformers/CommonJsTransformer.ts
+++ packages/ts-transpiler/src/transformers/CommonJsTransformer.ts
@@ -1,4 +1,4 @@
-import TypeScript from 'typescript'
+import TypeScript, { TypeChecker } from 'typescript'
 
 /*
 Transpile CommonJS to ES6 module
@@ -6,181 +6,360 @@ Transpile CommonJS to ES6 module
 export default class CommonJsTransformer
 	implements TypeScript.CustomTransformer {
 	private context: TypeScript.TransformationContext
+	private counter = 0
 	constructor(context: TypeScript.TransformationContext) {
 		this.context = context
 	}
-	private visit(node: TypeScript.Node): TypeScript.Node {
-		// Forward
-		if (
-			TypeScript.isExpressionStatement(node) &&
-			TypeScript.isBinaryExpression(node.expression) &&
-			node.expression.operatorToken.kind ===
-				TypeScript.SyntaxKind.EqualsToken &&
-			TypeScript.isPropertyAccessExpression(node.expression.left) &&
-			node.expression.left.name.getText() === 'exports' &&
-			node.expression.left.expression.getText() === 'module' &&
-			TypeScript.isCallExpression(node.expression.right) &&
-			TypeScript.isIdentifier(node.expression.right.expression) &&
-			node.expression.right.expression.getText() === 'require' &&
-			node.expression.right.arguments.length > 0 &&
-			TypeScript.isStringLiteral(node.expression.right.arguments[0])
-		) {
-			return TypeScript.factory.createExportDeclaration(
-				undefined,
-				undefined,
-				false,
-				undefined, // Wildcard
-				node.expression.right.arguments[0],
-			)
-			/*
-			return TypeScript.factory.createExportDeclaration(
-				undefined,
-				undefined,
-				false,
-				TypeScript.factory.createNamedExports([
-					TypeScript.factory.createExportSpecifier('*', 'default'),
-				]),
-				node.expression.right.arguments[0],
-			)
-			*/
-		}
-
-		// Import
-		if (
-			TypeScript.isExpressionStatement(node) &&
-			TypeScript.isCallExpression(node.expression) &&
-			TypeScript.isIdentifier(node.expression.expression) &&
-			node.expression.expression.getText() === 'require' &&
-			node.expression.arguments.length === 1
-		) {
-			const argument = node.expression.arguments[0]
-			if (TypeScript.isStringLiteralLike(argument)) {
-				return TypeScript.factory.createImportDeclaration(
-					undefined,
-					undefined,
-					undefined,
-					TypeScript.factory.createStringLiteral(argument.text),
-				)
-			}
-		}
-		// Default import
-		if (
-			TypeScript.isVariableStatement(node) &&
-			TypeScript.isVariableDeclarationList(node.declarationList) &&
-			node.declarationList.declarations.length === 1 &&
-			TypeScript.isVariableDeclaration(node.declarationList.declarations[0])
-		) {
-			const declaration = node.declarationList.declarations[0]
+	transformBundle(): TypeScript.Bundle {
+		throw new Error('Method not implemented.')
+	}
+	public transformSourceFile(
+		sourceFile: TypeScript.SourceFile,
+	): TypeScript.SourceFile {
+		const requireInTopScope = this.requireTopScope(sourceFile)
+		const withoutModule = this.stripModule(requireInTopScope)
+		const withoutDefineProperty = this.convertDefinePropery(withoutModule)
+		const esmExport = this.convertToEsmExport(withoutDefineProperty)
+		return this.convertToEsmImport(esmExport)
+	}
+	// Generate a file unique variable name
+	private generateUniqueName() {
+		this.counter++
+		return 'GENERATED_VAR_BY_TRANSFORMER_' + String(this.counter)
+	}
+	// Convert all Object.defineProperty(exports, "hello", Hello);"
+	private convertDefinePropery(
+		sourceFile: TypeScript.SourceFile,
+	): TypeScript.SourceFile {
+		const visit = (node: TypeScript.Node): TypeScript.Node => {
 			if (
-				TypeScript.isVariableDeclaration(declaration) &&
-				declaration.initializer &&
-				TypeScript.isCallExpression(declaration.initializer) &&
-				TypeScript.isIdentifier(declaration.initializer.expression) &&
-				declaration.initializer.expression.getText() === 'require' &&
-				declaration.initializer.arguments.length === 1
+				TypeScript.isExpressionStatement(node) &&
+				TypeScript.isCallExpression(node.expression) &&
+				TypeScript.isPropertyAccessExpression(node.expression.expression) &&
+				TypeScript.isIdentifier(node.expression.expression.expression) &&
+				TypeScript.isIdentifier(node.expression.expression.name) &&
+				node.expression.expression.expression.getText() === 'Object' &&
+				node.expression.expression.name.getText() === 'defineProperty' &&
+				TypeScript.isIdentifier(node.expression.arguments[0]) &&
+				node.expression.arguments[0].getText() === 'exports'
 			) {
-				const argument = declaration.initializer.arguments[0]
-				if (TypeScript.isStringLiteralLike(argument)) {
-					if (TypeScript.isIdentifier(declaration.name)) {
-						return TypeScript.factory.createImportDeclaration(
-							undefined,
-							undefined,
-							TypeScript.factory.createImportClause(
-								false,
-								declaration.name,
-								undefined,
+				const secondArgument = node.expression.arguments[1]
+				const thirdArgument = node.expression.arguments[2]
+				if (
+					TypeScript.isStringLiteral(secondArgument) &&
+					TypeScript.isIdentifier(thirdArgument)
+				) {
+					return TypeScript.factory.createExportDeclaration(
+						undefined,
+						undefined,
+						false,
+						TypeScript.factory.createNamedExports([
+							TypeScript.factory.createExportSpecifier(
+								thirdArgument,
+								secondArgument.text,
 							),
-							TypeScript.factory.createStringLiteral(argument.text),
-						)
-					} else if (TypeScript.isObjectBindingPattern(declaration.name)) {
-						return TypeScript.factory.createImportDeclaration(
-							undefined,
-							undefined,
-							TypeScript.factory.createImportClause(
-								false,
-								undefined,
-								TypeScript.factory.createNamedImports(
-									declaration.name.elements.map((bindingElement) =>
-										TypeScript.factory.createImportSpecifier(
-											bindingElement.propertyName as TypeScript.Identifier,
-											bindingElement.name as TypeScript.Identifier,
+						]),
+					)
+				}
+			}
+			return TypeScript.visitEachChild(node, visit, this.context)
+		}
+		return TypeScript.visitNode(sourceFile, visit)
+	}
+	// Top level CommonJS to ESM
+	private convertToEsmImport(
+		sourceFile: TypeScript.SourceFile,
+	): TypeScript.SourceFile {
+		const visit = (
+			node: TypeScript.Node,
+		): TypeScript.Node | TypeScript.Node[] => {
+			// Import without name
+			if (
+				TypeScript.isExpressionStatement(node) &&
+				TypeScript.isCallExpression(node.expression) &&
+				TypeScript.isIdentifier(node.expression.expression) &&
+				node.expression.expression.getText() === 'require' &&
+				node.expression.arguments.length === 1
+			) {
+				const argument = node.expression.arguments[0]
+				if (TypeScript.isStringLiteralLike(argument)) {
+					return TypeScript.factory.createImportDeclaration(
+						undefined,
+						undefined,
+						undefined,
+						TypeScript.factory.createStringLiteral(argument.text),
+					)
+				}
+			}
+			// Import with reference
+			if (
+				TypeScript.isVariableStatement(node) &&
+				TypeScript.isVariableDeclarationList(node.declarationList)
+			) {
+				const importDeclarations: TypeScript.ImportDeclaration[] = []
+				const variableDeclarations: TypeScript.VariableDeclaration[] = []
+				for (const variableDeclaration of node.declarationList.declarations) {
+					if (
+						variableDeclaration.initializer &&
+						TypeScript.isCallExpression(variableDeclaration.initializer) &&
+						TypeScript.isIdentifier(
+							variableDeclaration.initializer.expression,
+						) &&
+						variableDeclaration.initializer.expression.getText() ===
+							'require' &&
+						variableDeclaration.initializer.arguments.length === 1
+					) {
+						const argument = variableDeclaration.initializer.arguments[0]
+						if (
+							TypeScript.isStringLiteral(argument) &&
+							TypeScript.isIdentifier(variableDeclaration.name)
+						) {
+							importDeclarations.push(
+								TypeScript.factory.createImportDeclaration(
+									undefined,
+									undefined,
+									TypeScript.factory.createImportClause(
+										false,
+										undefined,
+										TypeScript.factory.createNamespaceImport(
+											variableDeclaration.name,
 										),
 									),
+									argument,
 								),
+							)
+						} else if (
+							TypeScript.isStringLiteral(argument) &&
+							TypeScript.isObjectBindingPattern(variableDeclaration.name)
+						) {
+							importDeclarations.push(
+								TypeScript.factory.createImportDeclaration(
+									undefined,
+									undefined,
+									TypeScript.factory.createImportClause(
+										false,
+										undefined,
+										TypeScript.factory.createNamedImports(
+											variableDeclaration.name.elements.map((bindingElement) =>
+												TypeScript.factory.createImportSpecifier(
+													bindingElement.propertyName as TypeScript.Identifier,
+													bindingElement.name as TypeScript.Identifier,
+												),
+											),
+										),
+									),
+									TypeScript.factory.createStringLiteral(argument.text),
+								),
+							)
+						}
+					} else {
+						variableDeclarations.push(variableDeclaration)
+					}
+				}
+				if (variableDeclarations.length === 0) {
+					return importDeclarations
+				} else {
+					return [
+						...importDeclarations,
+						TypeScript.factory.updateVariableStatement(
+							node,
+							undefined,
+							TypeScript.factory.updateVariableDeclarationList(
+								node.declarationList,
+								variableDeclarations,
 							),
-							TypeScript.factory.createStringLiteral(argument.text),
+						),
+					]
+				}
+			}
+			return node
+		}
+		return TypeScript.visitEachChild(sourceFile, visit, this.context)
+	}
+	// Top level CommonJS to ESM
+	private convertToEsmExport(
+		sourceFile: TypeScript.SourceFile,
+	): TypeScript.SourceFile {
+		const visit = (
+			node: TypeScript.Node,
+		): TypeScript.Node | TypeScript.Node[] => {
+			if (
+				TypeScript.isExpressionStatement(node) &&
+				TypeScript.isBinaryExpression(node.expression) &&
+				node.expression.operatorToken.kind === TypeScript.SyntaxKind.EqualsToken
+			) {
+				// exports.something = something;
+				if (
+					TypeScript.isPropertyAccessExpression(node.expression.left) &&
+					TypeScript.isIdentifier(node.expression.left.name) &&
+					TypeScript.isIdentifier(node.expression.left.expression) &&
+					node.expression.left.expression.getText() === 'exports'
+				) {
+					// exports.default = something;
+					if (node.expression.left.name.getText() === 'default') {
+						return TypeScript.factory.createExportAssignment(
+							undefined,
+							undefined,
+							undefined,
+							node.expression.right,
+						)
+					}
+					// exports.something = anIdentifier;
+					else if (TypeScript.isIdentifier(node.expression.right)) {
+						return TypeScript.factory.createExportDeclaration(
+							undefined,
+							undefined,
+							false,
+							TypeScript.factory.createNamedExports([
+								TypeScript.factory.createExportSpecifier(
+									node.expression.right,
+									node.expression.left.name,
+								),
+							]),
+						)
+					} else {
+						const newIdentifierName = this.generateUniqueName()
+						const variableStatement = TypeScript.factory.createVariableStatement(
+							undefined,
+							[
+								TypeScript.factory.createVariableDeclaration(
+									newIdentifierName,
+									undefined,
+									undefined,
+									node.expression.right,
+								),
+							],
+						)
+						return [
+							variableStatement,
+							TypeScript.factory.createExportDeclaration(
+								undefined,
+								undefined,
+								false,
+								TypeScript.factory.createNamedExports([
+									TypeScript.factory.createExportSpecifier(
+										TypeScript.factory.createIdentifier(newIdentifierName),
+										node.expression.left.name,
+									),
+								]),
+							),
+						]
+					}
+				}
+				// exports = something;
+				if (
+					TypeScript.isIdentifier(node.expression.left) &&
+					node.expression.left.getText() === 'exports'
+				) {
+					// exports = { a: false, b: true }
+					if (TypeScript.isObjectLiteralExpression(node.expression.right)) {
+						const exportSpecifiers: TypeScript.ExportSpecifier[] = []
+						for (const property of node.expression.right.properties) {
+							if (
+								TypeScript.isPropertyAssignment(property) &&
+								TypeScript.isIdentifier(property.name) &&
+								TypeScript.isIdentifier(property.initializer)
+							) {
+								exportSpecifiers.push(
+									TypeScript.factory.createExportSpecifier(
+										property.initializer,
+										property.name,
+									),
+								)
+							}
+						}
+						return TypeScript.factory.createExportDeclaration(
+							undefined,
+							undefined,
+							false,
+							TypeScript.factory.createNamedExports(exportSpecifiers),
+						)
+					}
+					// exports = require('hello.js');
+					else if (
+						TypeScript.isCallExpression(node.expression.right) &&
+						node.expression.right.expression.getText() === 'require' &&
+						TypeScript.isStringLiteral(node.expression.right.arguments[0])
+					) {
+						return TypeScript.factory.createExportDeclaration(
+							undefined,
+							undefined,
+							false,
+							undefined,
+							node.expression.right.arguments[0],
+						)
+					}
+					// exports = 'hello' or exports = Hello
+					else {
+						return TypeScript.factory.createExportAssignment(
+							undefined,
+							undefined,
+							undefined,
+							node.expression.right,
 						)
 					}
 				}
 			}
+			return node
 		}
-
-		// exports.default = Hello;
-		if (
-			TypeScript.isExpressionStatement(node) &&
-			TypeScript.isBinaryExpression(node.expression) &&
-			TypeScript.isPropertyAccessExpression(node.expression.left) &&
-			TypeScript.isIdentifier(node.expression.left.expression) &&
-			TypeScript.isIdentifier(node.expression.left.name) &&
-			node.expression.left.name.getText() === 'default' &&
-			node.expression.left.expression.getText() === 'exports'
-		) {
-			return TypeScript.factory.createExportAssignment(
-				undefined,
-				undefined,
-				undefined,
-				node.expression.right,
-			)
-		}
-
-		// module.exports = Hello;
-		if (
-			TypeScript.isExpressionStatement(node) &&
-			TypeScript.isBinaryExpression(node.expression) &&
-			TypeScript.isPropertyAccessExpression(node.expression.left) &&
-			TypeScript.isIdentifier(node.expression.left.expression) &&
-			TypeScript.isIdentifier(node.expression.left.name) &&
-			node.expression.left.name.getText() === 'exports' &&
-			node.expression.left.expression.getText() === 'module'
-		) {
-			return TypeScript.factory.createExportAssignment(
-				undefined,
-				undefined,
-				undefined,
-				node.expression.right,
-			)
-		}
-
-		// module.exports.default = Hello;
-		if (
-			TypeScript.isExpressionStatement(node) &&
-			TypeScript.isBinaryExpression(node.expression) &&
-			TypeScript.isPropertyAccessExpression(node.expression.left) &&
-			TypeScript.isIdentifier(node.expression.left.expression) &&
-			TypeScript.isIdentifier(node.expression.left.name) &&
-			node.expression.left.expression.getText() === 'exports' &&
-			TypeScript.isIdentifier(node.expression.left.name) &&
-			TypeScript.isIdentifier(node.expression.right)
-		) {
-			return TypeScript.factory.createExportDeclaration(
-				undefined,
-				undefined,
-				false,
-				TypeScript.factory.createNamedExports([
-					TypeScript.factory.createExportSpecifier(
-						node.expression.right,
-						node.expression.left.name,
-					),
-				]),
-			)
-		}
-
-		return node
+		return TypeScript.visitEachChild(sourceFile, visit, this.context)
 	}
-	transformSourceFile(node: TypeScript.SourceFile): TypeScript.SourceFile {
-		return TypeScript.visitEachChild(node, this.visit.bind(this), this.context)
+	// module.exports -> exports
+	private stripModule(
+		sourceFile: TypeScript.SourceFile,
+	): TypeScript.SourceFile {
+		const visit = (node: TypeScript.Node): TypeScript.Node => {
+			if (
+				TypeScript.isPropertyAccessExpression(node) &&
+				TypeScript.isIdentifier(node.expression) &&
+				TypeScript.isIdentifier(node.name) &&
+				node.expression.getText() === 'module' &&
+				node.name.getText() === 'exports'
+			) {
+				return node.name
+			}
+			return TypeScript.visitEachChild(node, visit, this.context)
+		}
+		return TypeScript.visitNode(sourceFile, visit)
 	}
-	transformBundle(node: TypeScript.Bundle): TypeScript.Bundle {
-		return node
+	// Move all require-calls to top-scope
+	private requireTopScope(
+		sourceFile: TypeScript.SourceFile,
+	): TypeScript.SourceFile {
+		const newStatements: TypeScript.VariableStatement[] = []
+		const visit = (node: TypeScript.Node): TypeScript.Node => {
+			const inRootScope =
+				node?.parent?.parent?.parent?.parent &&
+				TypeScript.isVariableDeclaration(node.parent) &&
+				TypeScript.isVariableDeclarationList(node.parent.parent) &&
+				TypeScript.isVariableStatement(node.parent.parent.parent) &&
+				TypeScript.isSourceFile(node.parent.parent.parent.parent)
+			if (
+				TypeScript.isCallExpression(node) &&
+				TypeScript.isIdentifier(node.expression) &&
+				node.arguments.length === 1 &&
+				node.expression.getText() === 'require' &&
+				inRootScope === false
+			) {
+				const newIdentifierName = this.generateUniqueName()
+				newStatements.push(
+					TypeScript.factory.createVariableStatement(undefined, [
+						TypeScript.factory.createVariableDeclaration(
+							newIdentifierName,
+							undefined,
+							undefined,
+							node,
+						),
+					]),
+				)
+				return TypeScript.factory.createIdentifier(newIdentifierName)
+			}
+			return TypeScript.visitEachChild(node, visit, this.context)
+		}
+		const changedSourceFile = TypeScript.visitNode(sourceFile, visit)
+		return TypeScript.factory.updateSourceFile(changedSourceFile, [
+			...newStatements,
+			...changedSourceFile.statements,
+		])
 	}
 }
