fdcf5382ad40e370d0e251f525d77d5b8a1f2c71
atyn <atynatyn@gmail.com>
2021-03-23 09:42:31 +0100

    Fixed an issue caused by previous commit

--- packages/ts-transpiler/src/index.ts
+++ packages/ts-transpiler/src/index.ts
@@ -1,4 +1,4 @@
-export { default as EnergyEIcon } from './TsTranspiler'
+export { default as TsTranspiler } from './TsTranspiler'
 export { default as CommonJsTransformer } from './transformers/CommonJsTransformer'
 export { default as NodeEnvTransformer } from './transformers/NodeEnvTransformer'
 export { default as ResolveTransformer } from './transformers/ResolveTransformer'
