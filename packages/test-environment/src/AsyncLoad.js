9fec3313c45ecb5917775fbe568be8426d81f163
atyn <atynatyn@gmail.com>
2021-04-01 08:35:04 +0200

    It now handle field "browser" in package.json

--- packages/test-environment/src/AsyncLoad.js
+++ packages/test-environment/src/AsyncLoad.js
@@ -2,5 +2,11 @@ import './ModuleD'
 
 export async function getSomething() {
 	const content = await import('./ModuleC')
+	const unified = await import('remark-parse')
+	// eslint-disable-next-line no-console
+	console.log('unified:', unified)
+	const remarkParse = await import('unified')
+	// eslint-disable-next-line no-console
+	console.log('remarkParse:', remarkParse)
 	return content.default
 }
